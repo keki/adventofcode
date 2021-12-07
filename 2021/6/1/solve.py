f82e66e519be170a33780b0f0471ac4df45140c5
Balázs Kéki <keki@kinja.com>
2021-12-08 00:50:20 +0100

    day 6 solution 1 cleanup

--- 2021/6/1/solve.py
+++ 2021/6/1/solve.py
@@ -4,19 +4,16 @@ import numpy as np
 input = np.loadtxt('input', dtype=int, delimiter=',')
 
 max_timer = 8
-timers = np.zeros(max_timer+1, dtype = int)
 days = 80
 
+# count unique values in input
+timers = np.zeros(max_timer+1, dtype = int)
 for i in input:
 	timers[i] += 1
 
-#print(input)
-#print("start:", timers)
-# every day shift one left, 0 goes to 8 and is added to 6
+# every day shift the array left and move times[0] to times[8], add previous [0] to [6]
 for day in range(1, days+1):
-	zeros = timers[0]
 	timers = np.roll(timers, -1)
-	timers[6] += zeros
-	#print(day , ':' , timers)
+	timers[6] += timers[8]
 
 print(np.sum(timers))
