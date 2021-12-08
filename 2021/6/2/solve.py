db28e65cc5ae8a35defaf6f4b7b2352bdf4320f8
Balázs Kéki <keki@kinja.com>
2021-12-08 01:54:53 +0100

    LOL

--- 2021/6/2/solve.py
+++ 2021/6/2/solve.py
@@ -12,7 +12,7 @@ for i in input:
 	timers[i] += 1
 
 # every day shift the array left and move times[0] to times[8], add previous [0] to [6]
-for day in range(1, days+1):
+for day in range(0, days):
 	timers = np.roll(timers, -1)
 	timers[6] += timers[8]
 
