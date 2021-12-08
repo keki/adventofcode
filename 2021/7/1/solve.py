773e584e0b2f726d2cee89a52eb4c32b8835ab5d
Balázs Kéki <keki@kinja.com>
2021-12-08 01:50:01 +0100

    day 7 solution 1 cleanup

--- 2021/7/1/solve.py
+++ 2021/7/1/solve.py
@@ -1,13 +1,14 @@
 import math
 import numpy as np
 
-input = np.loadtxt('input', dtype=int, delimiter=',')
+positions = np.loadtxt('testinput', dtype=int, delimiter=',')
 
-min_dist = np.min(input)
-max_dist = np.max(input)
+min_dist = np.min(positions)
+max_dist = np.max(positions)
 totals = np.zeros(max_dist+1, dtype = int)
 
 for align_position in range(min_dist, max_dist + 1):
-	total_fuel[align_position] = sum(abs(input - align_position))
+	distances = abs(positions - align_position)
+	totals[align_position] = sum(distances)
 
 print("smallest fuel need: ", np.min(totals))
