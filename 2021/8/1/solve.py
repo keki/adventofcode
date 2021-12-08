93923d03e10f6cebe92e9037f289d6839fcd3480
Balázs Kéki <keki@kinja.com>
2021-12-08 19:43:26 +0100

    cleanup

--- 2021/8/1/solve.py
+++ 2021/8/1/solve.py
@@ -2,11 +2,11 @@ import numpy as np
 
 data = np.loadtxt('input', dtype=str)
 
-unique_lengths = set([2,3,4,7])
+unique_code_lengths = set([2,3,4,7])
 outputs = data[:,11:]
 
-def code_lengths(a):
- return [len(i) in unique_lengths for i in a]
+def code_lengths(arr):
+ return [len(code) in unique_code_lengths for code in arr]
 
 codes_with_unique_lengths = np.apply_along_axis(code_lengths, 1, outputs)
 
