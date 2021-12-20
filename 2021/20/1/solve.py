5360836136721a4abb7fb845a15f6e58c164b3e0
Balázs Kéki <keki@kinja.com>
2021-12-20 13:22:04 +0100

    cleanup

--- 2021/20/1/solve.py
+++ 2021/20/1/solve.py
@@ -2,32 +2,32 @@ import numpy as np
 from os import system
 from time import sleep
 
-def pixelToInt(pixel):
+def read_input():
+    with open('input', 'r') as input_file:
+        data = []
+        scanner = []
+        algo = list(map(as_int,input_file.readline().strip()))
+        _ = input_file.readline() # read empty line
+        row = input_file.readline().strip()
+        image = []
+        while row:
+            image.append(list(map(as_int, row)))
+            row = input_file.readline().strip()
+        return (np.array(algo), np.array(image))
+
+def as_int(pixel):
     if pixel == '.':
         return 0
     else:
         return 1
 
-def intToPixel(int):
+def as_char(int):
     if int == 0:
         return '.'
     else:
         return "#"
 
-def read_input():
-    with open('input', 'r') as input_file:
-        data = []
-        scanner = []
-        algo = list(map(pixelToInt,input_file.readline().strip()))
-        _ = input_file.readline() # read empty line
-        row = input_file.readline().strip()
-        image = []
-        while row:
-            image.append(list(map(pixelToInt, row)))
-            row = input_file.readline().strip()
-        return (np.array(algo), np.array(image))
-
-def pixel_value(img, pos, outside_bit, algo):
+def bit_value(img, pos, outside_bit, algo):
     pv = 0
     for i in -1, 0 , 1:
         for j in -1, 0, 1:
@@ -48,35 +48,29 @@ def img_map(input_image, algo, outside_bit):
     outside_arr = np.full((3,3), outside_bit)
     image = np.zeros(input_image.shape, dtype=int)
 
-    new_outside_bit = pixel_value(outside_arr, (1,1), outside_bit, algo)
+    new_outside_bit = bit_value(outside_arr, (1,1), outside_bit, algo)
 
     for i, row in enumerate(image):
         for j, bit in enumerate(row):
-            image[i, j] = pixel_value(input_image, (i,j), outside_bit, algo)
+            image[i, j] = bit_value(input_image, (i,j), outside_bit, algo)
 
     return (image, new_outside_bit)
 
-def to_string(image):
-    def bit_to_hash(bit):
-        if(bit == 0):
-            return '.'
-        else:
-            return '#'
-
-    return "\n".join([''.join(list(map(bit_to_hash, row))) for row in image])
+def as_string(image):
+    return "\n".join([''.join(list(map(as_char, row))) for row in image])
 
 
 def main():
     algo, image = read_input()
 
     #print(algo)
-    #print(to_string(image))
+    print(as_string(image))
 
     outside_bit = 0
     for i in range(50):
         (image, outside_bit) = img_map(image, algo, outside_bit)
-        print(i)
-        #print(to_string(image))
+        system('clear')
+        print(as_string(image))
 
     print("LIT BITS:", np.sum(image))
 
